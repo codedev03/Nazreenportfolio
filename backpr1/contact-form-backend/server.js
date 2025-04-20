7981aca095e25d2b34d859324e10249cf7e76a7f
codedev03 <nazreenkmmmc@gmail.com>
2025-04-20 13:54:33 +0530

    Update CORS with deployed frontend URL

--- backpr1/contact-form-backend/server.js
+++ backpr1/contact-form-backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://frontendportfolio-nk4h.onrender.com/", // Replace with actual deployed frontend URL
+    methods: ["POST", "GET"],
+    credentials: true
+  }));
 app.use(bodyParser.json());
 
 // Root route for testing
